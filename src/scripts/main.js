fca0ea12fa2629715b51edc6a14de33dad740647
Nina <pinkangelssugar@gmail.com>
2020-04-16 15:49:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+const largeImg = document.querySelector('.gallery__large-img');
+const element = document.querySelector('.gallery__list');
+
+element.addEventListener('click', (eventer) => {
+  eventer.preventDefault();
+
+  const link = eventer.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
